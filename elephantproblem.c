1a107b49e7ccafa5a0d79d28f940babfe9832fe7
thelokeshgoel00 <40207268+thelokeshgoel00@users.noreply.github.com>
2018-11-13 17:58:39 +0530

    Initial commit

---
+++ elephantproblem.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ main()
+{
+      float n,arr[100],sum=0,output;
+      scanf("%f",&n);
+      for(int i=0;i<n;i++)
+      {
+          scanf("%f",&arr[i]);
+sum+=arr[i];
+      }
+output=sum/n;
+printf("%f",output);
+p
+
+}
